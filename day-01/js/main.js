894f52aa78860a7446559aed448447733dabbaf2
Akaljot Gujjar <akaljotgujjar@gmail.com>
2021-06-30 09:28:17 -0700

    take home challenge

--- day-01/js/main.js
+++ day-01/js/main.js
@@ -1,42 +1,46 @@
-const tag = 'button'; // string 
+const tag = "button"; // string
 const fontSize = 50; // number
-const isActive = true; // boolean 
+const isActive = true; // boolean
 
 const object = {
-    tag: 'h1', 
-    fontSize: '30px',
-    isActive: true
+  tag: "h1",
+  fontSize: "30px",
+  isActive: true,
 };
 
 const array = [tag, fontSize, isActive];
 
-
 // **how to create a button using javascript**
-// create an element 
+// create an element
 const differentButton = document.createElement(object.tag);
 
 // place element on the body
 document.body.appendChild(differentButton);
 
 // update the innerHTML of button
-differentButton.innerHTML = 'second button';
+differentButton.innerHTML = "second button";
 
-// create a variable that points to a different variable 
+// create a variable that points to a different variable
 
-const myButton = document.querySelector('#thisButton');
+const myButton = document.querySelector("#thisButton");
 // const myButton = differentButton;
 
 // changing styles on the button
-myButton.style.color = 'green';
-myButton.style.fontSize = '50px';
-
+myButton.style.color = "green";
+myButton.style.fontSize = "50px";
 
 //changing the text or html of a button
-myButton.innerText = 'Changed';
+myButton.innerText = "Changed";
 myButton.innerHTML = "Different";
 
-myButton.addEventListener('click', function() {
-    myButton.innerText = 'Test';
+myButton.addEventListener("click", function () {
+  myButton.innerText = "Test";
 });
 
 console.log(array);
+
+const fullName = "Akaljot Gujjar";
+const favFood = "Thai food";
+
+console.log(fullName);
+console.log(favFood);
