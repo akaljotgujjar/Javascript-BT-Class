89de4ff4b449b48ad462154f45b6ecbf44ac0057
Akaljot Gujjar <akaljotgujjar@gmail.com>
2021-07-06 20:50:54 -0700

    finished day 3

---
+++ day-03/js/daily-challenge.js
@@ -0,0 +1,22 @@
+// ** DAILY CHALLENGE** //
+
+// Ask for the user's first name
+// store their input to a variable
+// Reverse name
+// Alert the reversed name
+
+// Answer 1 //
+// const firstName = prompt("What is your first name?");
+// const firstNameFlipped = firstName.split("").reverse().join("");
+// alert(firstNameFlipped);
+
+// *BONUS*
+// Prompt for a number value (provide a default of 10)
+// Prompt for a second number value (provide a default of 10)
+// Convert the prompted values into integers using parseInt()
+// Add the numbers together and alert the user with the result
+
+// Answer 2 //
+// const number1 = parseInt(prompt("number 1?", 10));
+// const number2 = parseInt(prompt("number 2?", 10));
+// alert(number1 + number2);
---
+++ day-03/js/overview.js
@@ -0,0 +1,53 @@
+const yes = true;
+const no = false;
+
+const number1 = 1;
+const number2 = 2;
+const number3 = 3;
+
+const letterA = "a";
+const letterB = "b";
+const letterC = "c";
+
+const objects = {
+  property: yes,
+};
+
+// const user1 = {
+//     username: 'Akaljot',
+//     score = 100
+// };
+
+// const user2 = {
+//     username: 'Joe',
+//     score = 101
+// };
+
+// const array = [];
+// const users = [user1, user2];
+
+console.dir(document);
+
+function myFunction(text) {
+  alert(text);
+  return 1;
+}
+
+// myFunction('test');
+// myFunction('works');
+
+// const test1 = alert();
+// const test2 = confirm();
+// const test3 = prompt();
+
+// ** DAILY CHALLENGE** // 
+
+// Ask for the user's first name
+// Ask for the user's last name
+// Log the user's first name to the console
+// Alert the user's last name
+
+// *BONUS* 
+// Ask for the user's birthday
+// Ask the user to confirm their birthday input
+// Alert the user's birthday
